cd6e5902ceda3c71e6352593624329f7a2f8d7dc
Aymeric Fiorini <aymeric.fiorini@etu.univ-st-etienne.fr>
2021-03-22 16:26:07 +0100

    Initial commit

---
+++ src/main/java/de3/vanelle_fiorini/projet_big_data/ProjetBigDataApplication.java
@@ -0,0 +1,13 @@
+package de3.vanelle_fiorini.projet_big_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBigDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBigDataApplication.class, args);
+	}
+
+}
